75555cbe9b6deabbd43cc11055ae8f646a41ad0c
Ram Akasaka <142519662+ihell@users.noreply.github.com>
2024-11-29 17:12:05 +0700

    add: background and change: button color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
       fontFamily: {
         handwriting: ["'Patrick Hand', cursive"],
       },
+      backgroundImage: {
+        paper: "url('/public/paper-texture.jpg')", // Latar dengan tekstur kertas
+      },
+      colors: {
+        brown: {
+          500: "#8B4513", // Warna coklat terang (hover)
+          600: "#5D2E00", // Warna coklat gelap (utama)
+        },
+      },
     },
   },
   plugins: [],
